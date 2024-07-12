8cb4932cb57be90d0584542342851f4b32cf0bc1
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-07-12 16:04:43 +0200

    Fix: Fixed inexistent routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import Profile from '@components/Profile/Profile'
 import Error404 from '@components/Error/Error404'
 import CustomLayout from '@components/Layout/CustomLayout'
 import Dashboard from '@components/Dashboard/Dashboard'
+import Planner from '@components/Planner/Planner'
+import Maintenance from '@components/Maintenance/Maintenance'
+import Items from '@components/Items/Items'
+import Locations from '@components/Locations/Locations'
+import More from '@components/More/More'
 
 const router = createBrowserRouter([
   {
@@ -12,7 +17,12 @@ const router = createBrowserRouter([
     errorElement: <Error404 />,
     children: [
       {path: '/profile', element: <Profile />},
-      {path: '/dashboard', element: <Dashboard />}
+      {path: '/dashboard', element: <Dashboard />},
+      {path: '/planner', element: <Planner />},
+      {path: '/maintenance', element: <Maintenance />},
+      {path: '/items', element: <Items />},
+      {path: '/locations', element: <Locations />},
+      {path: '/more', element: <More />}
     ]
   }
 ])
