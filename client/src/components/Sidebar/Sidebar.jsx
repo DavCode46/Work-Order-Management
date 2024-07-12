123e67b0d2b004d5ed763dba95fdb84e2ebb9ecc
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-07-12 15:54:13 +0200

    Feat: sidebar done

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -15,57 +15,56 @@ import './sidebar.css';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar-header">
+      <div className="sidebar__header">
         <Link to="/profile">
           <Avatar size={40} style={{ backgroundColor: '#007bff' }}>DB</Avatar>
         </Link>
-        <div className="sidebar-title">wanderwhiskers</div>
       </div>
-      <ul className="sidebar-menu">
-        <li className="sidebar-item">
-          <Link to="/dashboard">
+      <ul className="sidebar__menu">
+        <li>
+          <Link to="/dashboard" className='sidebar__menu-item'>
             <MdOutlineDashboardCustomize className="sidebar-icon" />
             Dashboard
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/planner">
+        <li>
+          <Link to="/planner" className='sidebar__menu-item'>
             <MdOutlineEventNote className="sidebar-icon" />
             Planner
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/maintenance">
+        <li>
+          <Link to="/maintenance" className='sidebar__menu-item'>
             <MdOutlineBuild className="sidebar-icon" />
             Maintenance
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/meters">
+        <li>
+          <Link to="/meters" className='sidebar__menu-item'>
             <MdOutlineSpeed className="sidebar-icon" />
             Meters
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/items">
+        <li>
+          <Link to="/items" className='sidebar__menu-item'>
             <MdOutlineWidgets className="sidebar-icon" />
             Items
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/locations">
+        <li>
+          <Link to="/locations" className='sidebar__menu-item'>
             <MdOutlineLocationOn className="sidebar-icon" />
             Locations
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/reports">
+        <li>
+          <Link to="/reports" className='sidebar__menu-item'>
             <MdOutlineAssessment className="sidebar-icon" />
             Reports
           </Link>
         </li>
-        <li className="sidebar-item">
-          <Link to="/more">
+        <li>
+          <Link to="/more" className='sidebar__menu-item'>
             <MdOutlineMoreHoriz className="sidebar-icon" />
             More
           </Link>
